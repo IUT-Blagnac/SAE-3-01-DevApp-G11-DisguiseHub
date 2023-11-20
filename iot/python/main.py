67db27a9e097e6888949736924f25dfb7dd98a41
Maxence Krill <maxencekrill@gmail.com>
2023-11-21 00:16:17 +0100

    Avancement UNIX + fix fichier manquant Crée le fichier JSON si vide ou manquant Ajout d'un système d'ouverture des fichiers avec os.open() pour UNIX Support des emojis (non-testé) pour UNIX (du moins le programme ne crash pas) Corrections de la partie UNIX (non-testée car Barbara n'arrive pas à se connecter à MQTT)

--- iot/python/main.py
+++ iot/python/main.py
@@ -1,4 +1,7 @@
-import yaml, json, signal, threading, sys, time
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+import os, yaml, json, signal, threading, sys, time
 import paho.mqtt.client as mqtt
 
 # Connexion au MQTT et abonnement au topic
@@ -8,6 +11,11 @@ def on_connect(client, userdata, flags, rc):
 
 # Réception des données
 def on_message(client, userdata, msg):
+    # Vérification du topic
+    if(msg.topic == "AM107/by-room/undefined/data"):
+        print("❌ Salle non définie")
+        return
+    
     # Décodage du message
     payload = json.loads(msg.payload)
 
@@ -51,11 +59,15 @@ def on_message(client, userdata, msg):
 # Ecriture des données dans le fichier
 def write():
     # Ecriture des données dans le fichier
-    with open(config["ecriture"]["fichiers"]["data"] + ".json", "w") as file:
-        json.dump(data, file)
-    with open(config["ecriture"]["fichiers"]["alerte"] + ".json", "w") as file:
-        json.dump(alerte, file)
-
+    if (sys.platform.startswith("win")):
+        with open(config["ecriture"]["fichiers"]["data"] + ".json", "w") as file:
+            json.dump(data, file)
+        with open(config["ecriture"]["fichiers"]["alerte"] + ".json", "w") as file:
+            json.dump(alerte, file)
+    elif (sys.platform.startswith("linux")):
+        os.write(os.open(config["ecriture"]["fichiers"]["data"] + ".json", os.O_WRONLY | os.O_TRUNC), json.dumps(data).encode())
+        os.write(os.open(config["ecriture"]["fichiers"]["alerte"] + ".json", os.O_WRONLY | os.O_TRUNC), json.dumps(alerte).encode())
+    
     # Rafraichissement des données
     dataload()
 
@@ -72,12 +84,45 @@ def write():
 # Lecture des données
 def dataload():
     global config, data, alerte
-    with open("config.yaml", "r") as file:
-        config = yaml.safe_load(file)
-    with open(config["ecriture"]["fichiers"]["data"] + ".json", "r") as file:
-        data = json.load(file)
-    with open(config["ecriture"]["fichiers"]["alerte"] + ".json", "r") as file:
-        alerte = json.load(file)
+
+    if os.path.exists("config.yaml"):
+        if (sys.platform.startswith("win")):
+            with open("config.yaml", "r") as file:
+                config = yaml.safe_load(file)
+        elif (sys.platform.startswith("linux")):
+            config_fd = os.open("config.yaml", os.O_RDONLY)
+            config_content = os.read(config_fd, os.path.getsize("config.yaml"))
+            os.close(config_fd)
+            config = yaml.safe_load(config_content)
+    else:
+        print("❌ Fichier de configuration introuvable")
+        exit()
+
+    data = readfile(config["ecriture"]["fichiers"]["data"] + ".json")
+    
+    alerte = readfile(config["ecriture"]["fichiers"]["data"] + ".json")
+
+# Lecture du fichier, création si inexistant, retourne le contenu
+def readfile(file):
+    if (sys.platform.startswith("win")):
+        if os.path.exists(file):
+            if os.path.getsize(file) > 0:
+                with open(file, "r") as file:
+                    data = json.load(file)
+            else:
+                data = {}
+                with open(file, "w") as file:
+                    json.dump(data, file)
+        else:
+            data = {}
+            with open(file, "w") as file:
+                json.dump(data, file)
+    elif (sys.platform.startswith("linux")):
+        data_fd = os.open(file, os.O_RDONLY)
+        data_content = os.read(data_fd, os.path.getsize(file))
+        os.close(data_fd)
+        data = json.loads(data_content)
+    return data
 
 # Initialisation des variables
 dataload()

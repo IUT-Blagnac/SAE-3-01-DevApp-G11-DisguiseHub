9c5a906ccad15e3aeb2ff7346f553dc9f6bb7925
Maxence Krill <maxencekrill@gmail.com>
2023-11-26 13:33:43 +0100

    Header/footer site web + Cleaning Ajout du header/footer du site web Modification des diagrammes de séquences systèmes Suppression de l'ancienne application Python IOT (archivée sur le drive)

---
+++ site/include/footer.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="liens">
+        <div>
+            <h2>Nous contacter</h2>
+            <a href="/aide.php">Aide et contact</a>
+            <a href="https://x.com/">X (Twitter)</a>
+            <a href="https://instagram.com/">Instagram</a>
+        </div>
+        <div>
+            <h2>Nous magasins</h2>
+            <a href="/magasins/paris.php">Paris</a>
+            <a href="/magasins/toulouse.php">Toulouse</a>
+            <a href="/magasins">Autres</a>
+        </div>
+        <div>
+            <h2>À propos</h2>
+            <a href="/plan-du-site.php">Plan du site</a>
+            <a href="/conditions-utilisations.php">Conditions d'utilisation</a>
+            <a href="/donnees.php">Protection des données</a>
+            <a href="/cookies.php">Navigateur et cookies</a>
+        </div>
+    </div>
+    <div class="copyright">
+        <p>© 2024 Disguise'Hub. Tous droits réservés.</p>
+        <a href="/magm.php">Réalisé par MAGM</a>
+    </div>
+</footer>

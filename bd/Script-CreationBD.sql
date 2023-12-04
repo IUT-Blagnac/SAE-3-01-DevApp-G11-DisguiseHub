4ba6bf04123cc06367322bef7e50b71e6c43f041
GeloSwift <angelo.nichele317@gmail.com>
2023-12-04 15:48:06 +0100

    Ajout du script de création de la BD
    Les triggeurs remplacent les contraintes CHECK qui n'existent pas sur MySQL

---
+++ bd/Script-CreationBD.sql
@@ -0,0 +1,214 @@
+DROP TABLE IF EXISTS CategTaille;
+DROP TABLE IF EXISTS GuideTaille;
+DROP TABLE IF EXISTS Avis;
+DROP TABLE IF EXISTS Commander;
+DROP TABLE IF EXISTS Commande;
+DROP TABLE IF EXISTS Produit;
+DROP TABLE IF EXISTS Categorie;
+DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS Paiement;
+DROP TABLE IF EXISTS VirementBancaire;
+DROP TABLE IF EXISTS Paypal;
+DROP TABLE IF EXISTS Cartebleue;
+
+CREATE TABLE Cartebleue (
+    numCB CHAR(16),
+    nomSurCB VARCHAR(30),
+    dateExpCB DATE,
+    codeSecuriteCB DECIMAL(3),
+    PRIMARY KEY (numCB)
+) Engine=InnoDB;
+
+CREATE TABLE Paypal (
+    idPaypal DECIMAL,
+    PRIMARY KEY (idPaypal)
+) Engine=InnoDB;
+
+CREATE TABLE VirementBancaire (
+    idVirement DECIMAL,
+    PRIMARY KEY (idVirement)
+) Engine=InnoDB;
+
+CREATE TABLE Paiement (
+    idPaiement DECIMAL,
+    numCB CHAR(16),
+    idPaypal DECIMAL,
+    idVirement DECIMAL,
+    PRIMARY KEY (idPaiement),
+    FOREIGN KEY (numCB) REFERENCES Cartebleue(numCB),
+    FOREIGN KEY (idPaypal) REFERENCES Paypal(idPaypal),
+    FOREIGN KEY (idVirement) REFERENCES VirementBancaire(idVirement)
+) Engine=InnoDB;
+
+CREATE TABLE Client (
+    numClient DECIMAL,
+    numCB CHAR(16),
+    nomClient VARCHAR(30),
+    prenomClient VARCHAR(30),
+    adresseClient VARCHAR(30),
+    mailClient VARCHAR(30),
+    codePostalClient DECIMAL(5),
+    villeClient VARCHAR(30),
+    civiliteClient VARCHAR(30),
+    dateNaissanceClient DATE,
+    telClient CHAR(10),
+    mdpClient VARCHAR(30),
+    PRIMARY KEY (numClient),
+    UNIQUE (mailClient),
+    UNIQUE (telClient),
+    FOREIGN KEY (numCB) REFERENCES Cartebleue(numCB)
+) Engine=InnoDB;
+
+CREATE TABLE Categorie (
+    idCategorie DECIMAL,
+    idCategoriePere DECIMAL,
+    nomCategorie VARCHAR(30),
+    PRIMARY KEY (idCategorie),
+    FOREIGN KEY (idCategoriePere) REFERENCES Categorie(idCategorie)
+) Engine=InnoDB;
+
+CREATE TABLE Produit (
+    refProduit VARCHAR(30),
+    idCategorie DECIMAL,
+    nomProduit VARCHAR(50),
+    descProduit VARCHAR,
+    prixProduit DECIMAL(6,2),
+    qteProduit DECIMAL,
+    tailleProduit VARCHAR(3),
+    couleurProduit VARCHAR(30),
+    PRIMARY KEY (refProduit),
+    FOREIGN KEY (idCategorie) REFERENCES Categorie(idCategorie)
+) Engine=InnoDB;
+
+CREATE TABLE Commande (
+    idCommande DECIMAL,
+    numClient DECIMAL,
+    idPaiement DECIMAL,
+    dateCommande DATE,
+    fraisLivraison DECIMAL(6,2),
+    adrLivraison VARCHAR(30),
+    codePostalLivraison DECIMAL(5),
+    statutCommande VARCHAR,
+    PRIMARY KEY (idCommande),
+    FOREIGN KEY (numClient) REFERENCES Client(numClient),
+    FOREIGN KEY (idPaiement) REFERENCES Paiement(idPaiement)
+) Engine=InnoDB;
+
+CREATE TABLE Commander (
+    refProduit VARCHAR(30),
+    idCommande DECIMAL,
+    qteCommandee DECIMAL,
+    PRIMARY KEY (refProduit, idCommande),
+    FOREIGN KEY (refProduit) REFERENCES Produit(refProduit),
+    FOREIGN KEY (idCommande) REFERENCES Commande(idCommande)
+) Engine=InnoDB;
+
+CREATE TABLE Avis (
+    idAvis DECIMAL,
+    idAvisPere DECIMAL,
+    refProduit VARCHAR(30),
+    numClient DECIMAL,
+    commentaire VARCHAR,
+    note DECIMAL(1),
+    PRIMARY KEY (idAvis),
+    FOREIGN KEY (idAvisPere) REFERENCES Avis(idAvis),
+    FOREIGN KEY (refProduit) REFERENCES Produit(refProduit),
+    FOREIGN KEY (numClient) REFERENCES Client(numClient)
+) Engine=InnoDB;
+
+CREATE TABLE GuideTaille (
+    idTaille DECIMAL,
+    tourPoitrine DECIMAL(6,2),
+    tourTaille DECIMAL(6,2),
+    tourBassin DECIMAL(6,2),
+    tailleFR VARCHAR(3),
+    tailleACommander VARCHAR(3),
+    PRIMARY KEY (idTaille)
+) Engine=InnoDB;
+
+CREATE TABLE CategTaille (
+    idTaille DECIMAL,
+    refProduit VARCHAR(30),
+    tailleSelonAgeEtSexe VARCHAR(3),
+    PRIMARY KEY (idTaille, refProduit),
+    FOREIGN KEY (idTaille) REFERENCES GuideTaille(idTaille),
+    FOREIGN KEY (refProduit) REFERENCES Produit(refProduit)
+) Engine=InnoDB;
+
+
+CREATE TRIGGER check_dateExpCB
+BEFORE INSERT ON Cartebleue
+FOR EACH ROW
+BEGIN
+    IF NEW.dateExpCB <> DATE_FORMAT(NEW.dateExpCB, '%d/%m/%Y') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La date d''expiration de la carte bleue doit être au format DD/MM/YYYY.';
+    END IF;
+END;
+//
+
+CREATE TRIGGER check_dateNaissanceClient
+BEFORE INSERT ON Client
+FOR EACH ROW
+BEGIN
+    IF NEW.dateNaissanceClient <> DATE_FORMAT(NEW.dateNaissanceClient, '%d/%m/%Y') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La date de naissance du client doit être au format DD/MM/YYYY.';
+    END IF;
+END;
+//
+
+CREATE TRIGGER check_tailleProduit
+BEFORE INSERT ON Produit
+FOR EACH ROW
+BEGIN
+    IF NEW.tailleProduit NOT IN ('XS', 'S', 'M', 'L', 'XL', 'XXL') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La taille du produit doit être XS, S, M, L, XL ou XXL.';
+    END IF;
+END;
+//
+
+CREATE TRIGGER check_dateCommande
+BEFORE INSERT ON Commande
+FOR EACH ROW
+BEGIN
+    IF NEW.dateCommande <> DATE_FORMAT(NEW.dateCommande, '%d/%m/%Y') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La date de commande doit être au format DD/MM/YYYY.';
+    END IF;
+END;
+//
+
+CREATE TRIGGER check_note
+BEFORE INSERT ON Avis
+FOR EACH ROW
+BEGIN
+    IF NEW.note NOT BETWEEN 0 AND 5 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La note doit être comprise entre 0 et 5.';
+    END IF;
+END;
+//
+
+CREATE TRIGGER check_tailleFR_and_tailleACommander
+BEFORE INSERT ON GuideTaille
+FOR EACH ROW
+BEGIN
+    IF NEW.tailleFR NOT IN ('XS', 'S', 'M', 'L', 'XL', 'XXL') OR NEW.tailleACommander NOT IN ('XS', 'S', 'M', 'L', 'XL', 'XXL') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Les tailles FR et à commander doivent être XS, S, M, L, XL ou XXL.';
+    END IF;
+END;
+//
+
+CREATE TRIGGER check_tailleSelonAgeEtSexe
+BEFORE INSERT ON CategTaille
+FOR EACH ROW
+BEGIN
+    IF NEW.tailleSelonAgeEtSexe NOT IN ('XS', 'S', 'M', 'L', 'XL', 'XXL') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La taille selon l''âge et le sexe doit être XS, S, M, L, XL ou XXL.';
+    END IF;
+END;
+//

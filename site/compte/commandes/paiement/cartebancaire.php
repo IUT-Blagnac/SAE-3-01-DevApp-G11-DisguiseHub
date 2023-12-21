ccd86bc7c7b176a4e91f8780d0541505c9832fdf
Guychel <guychel.babela@etu.univ-tlse2.fr>
2023-12-21 21:55:15 +0100

    page cartebancaire.php est fini page moyenpaielent.php est fini

--- site/compte/commandes/paiement/cartebancaire.php
+++ site/compte/commandes/paiement/cartebancaire.php
@@ -15,15 +15,49 @@
     <?php
     // Vérifier si le formulaire a été soumis
     if (isset($_POST["valider"])) {
-        // Récupérer la valeur du formulaire
-        $numCarte = $_POST["numCarte"];
+    // Récupérer la valeur du formulaire
+    $numCarte = $_POST["numCarte"];
+    $nomSurCB = $_POST["nomCarte"];
+    $dateExpCB = $_POST["dateExpiration"];
+    $parsedDate = date_parse_from_format("Y-m", $dateExpCB);
 
-        $numCarte = substr($_POST["numCarte"], 0, 16);
-        // Préparer et exécuter la requête SQL pour insérer les données dans la base de données
-        $sql = "INSERT INTO Paiement (numCB) VALUES (:numCarte)";
+    // Extraire le mois et l'année
+    $mois = sprintf("%02d", $parsedDate['month']); // Ajoute un zéro devant si le mois a un seul chiffre
+    $annee = substr($parsedDate['year'], -2); // Les deux derniers chiffres de l'année
 
-        $stmt = $conn->prepare($sql);
-        $succes = $stmt->execute(["numCarte" => htmlspecialchars($numCarte)]);
+    // Concaténer le résultat au format souhaité
+    $dateExpCB = $mois . '/' . $annee;
+    $codeSecuriteCB = $_POST["Cryptogramme"];
+
+    $numCarte = substr($_POST["numCarte"], 0, 16);
+
+    // Vérifier si la carte bleue existe déjà dans la table Cartebleue
+    $sqlCheckCarte = "SELECT * FROM Cartebleue WHERE numCB = :numCarte";
+    $stmtCheckCarte = $conn->prepare($sqlCheckCarte);
+    $stmtCheckCarte->execute(["numCarte" => htmlspecialchars($numCarte)]);
+
+    // Si la carte bleue existe, récupérer son ID, sinon l'insérer dans la table Cartebleue
+    if ($row = $stmtCheckCarte->fetch(PDO::FETCH_ASSOC)) {
+        $numCB = $row['numCB'];
+    } else {
+        // Insérer la carte bleue dans la table Cartebleue
+        $sqlInsertCarte = "INSERT INTO Cartebleue (numCB, nomSurCB, dateExpCB, codeSecuriteCB) VALUES (:numCarte, :nomSurCB, :dateExpCB, :codeSecuriteCB)";
+        $stmtInsertCarte = $conn->prepare($sqlInsertCarte);
+        $stmtInsertCarte->bindParam(':numCarte', $numCarte);
+        $stmtInsertCarte->bindParam(':nomSurCB', $nomSurCB);
+        $stmtInsertCarte->bindParam(':dateExpCB', $dateExpCB);
+        $stmtInsertCarte->bindParam(':codeSecuriteCB', $codeSecuriteCB);
+        $stmtInsertCarte->execute();
+
+        // Récupérer l'ID généré pour la carte bleue
+        $numCB = $conn->lastInsertId();
+    }
+
+    // Préparer et exécuter la requête SQL pour insérer les données dans la base de données
+    $sql = "INSERT INTO Paiement (numCB) VALUES (:numCarte)";
+
+    $stmt = $conn->prepare($sql);
+    $succes = $stmt->execute(["numCarte" => htmlspecialchars($numCarte)]);
     }
 
     ?>
@@ -44,7 +78,7 @@
             <input type="month" name="dateExpiration">
 
             <label class="subtitle"><b>Cryptogramme</b></label>
-            <input type="text" minlength="3" maxlength="3" pattern="[0-9]" title="Cryptogramme" name="Cryptogramme">
+            <input type="tel" minlength="3" maxlength="3" pattern="[0-9\s]{3,3}" title="Cryptogramme" name="Cryptogramme" autocomplete="cc-csc" placeholder="XXX" required />
 
             <button type="submit" name="valider" value="Valider">valider</button>
 

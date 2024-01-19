5df2b06cb35301ef9d602d1791f00f5092ed3474
Guychel <122049940+Guychelove@users.noreply.github.com>
2024-01-19 20:36:25 +0100

    Update SceneController.java

--- iot/javafx/src/main/java/tools/SceneController.java
+++ iot/javafx/src/main/java/tools/SceneController.java
@@ -3,7 +3,25 @@ package tools;
 import control.SceneBuilder;
 import javafx.stage.Stage;
 
+/**
+ * L'interface SceneController définit les méthodes nécessaires pour la gestion
+ * des scènes dans une application JavaFX.
+ */
 public interface SceneController {
+    
+    /**
+     * Initialise le contexte du contrôleur de scène.
+     *
+     * @param controller   Le constructeur de scène permettant la construction
+     *                     des différentes scènes de l'application.
+     * @param primaryStage La fenêtre principale de l'application.
+     */
     public void initContext(SceneBuilder controller, Stage primaryStage);
+    
+    /**
+     * Affiche une boîte de dialogue ou une fenêtre modale à l'utilisateur.
+     * Cette méthode peut être implémentée pour afficher des informations,
+     * des avertissements ou des demandes de l'utilisateur.
+     */
     public void displayDialog();
 }
